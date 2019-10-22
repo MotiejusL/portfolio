172d53384692cddadf50f30f331b08b6567ca353
Motiejus <motiejus.labzentis@gmail.com>
2019-10-22 23:59:57 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          'postcss-sass-loader',
+        ],
+      },
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
